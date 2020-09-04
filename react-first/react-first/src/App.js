6a57aab2017c2dc090345573655595bfaf16a52d
Yashi <yashiajmera11@gmail.com>
2020-09-05 00:28:38 +0530

    Completed upto hook

---
+++ react-first/react-first/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const App = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Yashi', age: 22 },
+      { name: 'Manu', age: 29 },
+  
+    ]
+  });
+
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    setPersonsState({
+      persons: [
+        { name: 'Aashi', age: 24 },
+        { name: 'Manu', age: 29 },
+      
+      ]
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Racing
+      </Person>
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+};
+
+export default App;
