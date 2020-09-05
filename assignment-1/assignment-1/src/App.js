b8754b1c4099f53f73941f4a7234cfb7471a3e1c
Yashi <yashiajmera11@gmail.com>
2020-09-05 11:32:36 +0530

    Assignment 1

---
+++ assignment-1/assignment-1/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const style={
+    color: 'green',
+
+  }
+  return (
+    <div className="App">
+      <h1>Hello World..!!!</h1>
+      <p style={style}>This is the first React Assignment....</p>
+    </div>
+  );
+}
+
+export default App;
