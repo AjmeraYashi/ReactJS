10d1603ab7a2ac08a5ae3c5360913d1ad361549b
Yashi <yashiajmera11@gmail.com>
2020-09-10 01:13:11 +0530

    assignment-3

--- assignment-3/assignment-3/src/App.js
+++ assignment-3/assignment-3/src/App.js
@@ -1,5 +1,5 @@
-
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,98 +9,113 @@ class App extends Component {
     super();
     this.state={
       employeeList : [
-        
+       
       ],
-      addEmployee : false,
+      addEmployee : false
     }
     
   }
+
+
   addEmployee() {
       this.setState({
           
           addEmployee : true,
-          editIndex:null,
         })
   }
 
 
-  addEmployeeData(event){
-    
+  addEmployeeData(){
+
     const name = document.getElementById('name').value;
     const job = document.getElementById('job').value;
-    
+  
     this.state.employeeList.push(
       {
         name:name,
-        job:job
+        job: job,
+
       },
     )
-    console.log(name);
       this.setState({
         addEmployee : false,
       })
   }
 
 
-  deleteEmployee=(event, index) =>{
-    const emp = this.state.employeeList.slice()
-    emp.splice(index, 1)
-    this.setState({  emp })
-}
+  deleteEmployee(index){
+   const empList = this.state.employeeList;
+    empList.splice(index, 1)
+   this.setState({
+     empList
+   })
+  }
+    
 
   renderDisplay(){
     return(
-      <div>
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-            <th>Remove</th>
-          </tr>
-          {this.state.employeeList.map((value,index)=>{
-            return(
-              <tr>
-                <td>{value.name}</td>
-                <td>{value.job}</td>
-                <td><button onClick={()=>{
+      
+        <div>
+          <h1>Employee List</h1>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th >Name</th>
+                  <th >Job</th>
+                 
+                  <th >Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+              {this.state.employeeList.map((value, index)=>{
+                    return(
+                       <tr>
+                            <td>{value.name}</td>
+                            <td >{value.job}</td>
+                        
+                            <td ><button onClick={()=>{
                               this.deleteEmployee(index)
                             }}>Delete</button></td>
-                </tr>
-            )
-            
-          })}
-        </table>
-      </div>
+                          </tr>
+                         
+                    )
+                  })}
+              </tbody>
+            </table>
+          </div>
+        </div>
     )
+    
   }
+
   renderAddEmployee() {
     return(
-      <div className="loginForm">
+      <div>
         <h1>Add New Employee</h1>
         <form>
-          <label >First Name</label>
-          <input type="text" id="name" placeholder="Enter Name"/>
-  
+        <div>
+          <label >Name</label>
+          <input type="text"  id="name"  placeholder="Enter Name"/>
+        </div>
+        <div>
           <label >Job</label>
-          <input type="text" id="job" placeholder="Enter Job Title"/>
-        
-
+          <input type="text"  id="job" placeholder="Enter Job Title"/>
+        </div>
+        </form>
         <button onClick={()=>{
         this.addEmployeeData()
         }}>Submit</button>
-        </form>
-        
       </div>
     )
   }
+
   render() {
     return (
       
       <div className="App">
-        {this.renderDisplay()}
-      {this.addEmployee && this.renderAddEmployee()}
-      
-
+      { this.renderDisplay()}
+      { this.renderAddEmployee()} 
       </div>
     );
   }
